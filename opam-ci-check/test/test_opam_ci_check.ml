f4ee4e3cc9480c1a1f5fb3b8986e126e397bc744
Puneeth Chaganti <punchagan@muse-amuse.in>
2024-10-25 22:09:21 +0530

    Use alcotest for opam-ci-check test(s)

--- opam-ci-check/test/test_opam_ci_check.ml
+++ opam-ci-check/test/test_opam_ci_check.ml
@@ -4,8 +4,19 @@
 
 open Opam_ci_check
 
-let () =
-  (* Detect collision modulo ('-' | '_' | '') *)
-  assert (Lint.Checks.package_name_collision "foo_barbaz" "foo-bar-baz");
-  (* No collision otherwise *)
-  assert (not @@ Lint.Checks.package_name_collision "foo-barbaz" "eoocar_caz")
+let test_package_name_collision =
+  let check p0 p1 expected =
+    let test_name = Printf.sprintf "package_name_collision %s %s" p0 p1 in
+    let got = Lint.Checks.package_name_collision p0 p1 in
+    let test_fun () = Alcotest.(check bool) p0 expected got in
+    (test_name, `Quick, test_fun)
+  in
+  [
+    (* Detect collision modulo ('-' | '_' | '') *)
+    check "foo_barbaz" "foo-bar-baz" true;
+    (* No collision otherwise *)
+    check "foo-barbaz" "eoocar_caz" false;
+  ]
+
+let collision_suite = ("Collision", test_package_name_collision)
+let () = Alcotest.run "opam-ci-check" [ collision_suite ]
